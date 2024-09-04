c37a6ef89856ade5771c2b189a664f046de33da2
Piero Capelo <piero.capelo@proton.me>
2024-09-04 14:04:35 -0300

    WIP postgres driver

---
+++ driver.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "root"
+	password = "password"
+	dbname   = "university"
+)
+
+func connect() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func main() {
+	db := connect()
+	defer db.Close()
+}
