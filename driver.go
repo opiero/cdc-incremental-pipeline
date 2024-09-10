36bcb0b9d03070e08c04fdd4a894f296060044f7
Piero Capelo <piero.capelo@proton.me>
2024-09-10 09:49:37 -0300

    now the driver creates the table

--- driver.go
+++ driver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -15,24 +16,40 @@ const (
 	dbname   = "university"
 )
 
-func connect() *sql.DB {
+func connect() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return db
+
+	return db, nil
 }
 
 func main() {
-	db := connect()
+	db, err := connect()
+	if err != nil {
+		panic(err)
+	}
 	defer db.Close()
+
+	content, err := os.ReadFile("resources/sql/create_table.sql")
+	if err != nil {
+		panic(err)
+	}
+
+	create_table := string(content)
+	result, err := db.Exec(create_table)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("result: %v\n", result)
 }
--- resources/sql/create_table.sql
+++ resources/sql/create_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    birthdate DATE NULL,
+    birthdate DATE NULL
 );
